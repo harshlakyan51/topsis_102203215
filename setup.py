efc719232fb0a8938e2474f63870ed355df44244
harshlakyan51 <149249720+harshlakyan51@users.noreply.github.com>
2025-01-19 19:21:20 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 # Setup configuration
 setup(
     name='Topsis-Harsh-102203215',  
-    version='1.1.4',  
+    version='1.4.9',  
     packages=find_packages(),  
     license='MIT',
     description='A Python package for TOPSIS ranking with CLI support for CSV/Excel files',
@@ -16,30 +16,14 @@ setup(
     long_description_content_type='text/markdown',
     author="Harsh Lakyan",  
     author_email='harshlakyan@gmail.com',
-    keywords=['topsis', 'multi-criteria decision making', 'python', 'pypi', 'csv', 'xlsx', 'cli'],
     install_requires=[
         'numpy>=1.21.0',  
         'pandas>=1.3.0',
     ],
     python_requires='>=3.6',  
     entry_points={
-        'console_scripts': [
-            'topsis = topsis.topsis_102203215',  
+        "console_scripts": [
+            "topsis=topsis.topsis_102203215:main",
         ],
     },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
 )
